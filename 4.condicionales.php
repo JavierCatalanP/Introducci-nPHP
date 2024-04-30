7f1415c4be8d58f0e16d6094ed8623c9fee46ec7
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-04-30 15:19:48 -0400

    Funciones con PHP

---
+++ 4.condicionales.php
@@ -0,0 +1,19 @@
+<?php
+echo 'Condicionales entre 2 variables <br>';
+
+$a = 10;
+$b = 20;
+
+if( $a > $b ) {
+    echo $a.' Es mayor que: '.$b;
+} 
+
+else if($a == $b) {
+    echo $a.' Es igual que: '.$b;
+}
+else {
+    echo $a.' Es menor que: '.$b;
+}
+
+
+?>
