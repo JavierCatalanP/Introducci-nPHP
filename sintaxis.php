78fdd6df2282449643db32235f7b68951d07b07a
JavierCatalanP <ingaciocatalanp@gmail.com>
2024-04-30 10:53:29 -0400

    Sintaxis en PHP

---
+++ sintaxis.php
@@ -0,0 +1,14 @@
+<?php
+// Comentarios con doble barra en PHP
+
+print "<h1>Hola Mundo con PHP</h1>";
+
+echo '<br id="salto"> Aprendiendo PHP'; // Se recominda usar comillas simples por los ID
+
+//Diferencias entre print y echo, print permite imprimir variables  y echo no, echo permite imprimir varias lineas de texto y print solo una.
+echo '<h1>Aprendiendo PHP</h1>', '<br> <h3>vamos a aprender POO</h3>' ;
+
+print "Hola Mundo";
+echo "Hola Mundo";
+
+?>
